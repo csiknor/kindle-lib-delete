bc546e6b19b1cb1b64c97025623e307add731a98
csiknor <norbert.csik@gmail.com>
2013-01-23 09:46:40 +0100

    Update check-delete.js

--- check-delete.js
+++ check-delete.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $('#ordersSortingRow .firstCol').append('<input type="checkbox" name="asinAll">').click(function(e) {
-  $('input[name=asin]').each(function(i, item) {
-	  item.checked = e.target.checked;
-	});
+    $('input[name=asin]').each(function(i, item) {
+      item.checked = e.target.checked;
+    });
   });
   $('.firstCol').css('width', 'auto');
   $('.rowBodyCollapsed').each(function(i){
